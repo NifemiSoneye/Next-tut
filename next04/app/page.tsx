01c84a882cff852b0e85851421cb3d0a79029529
NifemiSoneye <nifemisoneye@gmail.com>
2026-04-15 12:04:58 -0700

    Blog Project refactor

--- next04/app/page.tsx
+++ next04/app/page.tsx
@@ -2,20 +2,23 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Posts from "./components/Posts";
+import MyProfilePic from "./components/MyProfilePic";
 const inter = Inter({ subsets: ["latin"] });
 
 export const revalidate = 10;
 
 export default function Home() {
   return (
-    <main className="px-6 mx-auto">
+    <div className="mx-auto">
+      <MyProfilePic />
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">Dave</span>.
+          I'm <span className="font-bold">Dave</span>.
         </span>
       </p>
+      {/*@ts-expect-error Server Component */}
       <Posts />
-    </main>
+    </div>
   );
 }
--- next04/lib/posts.tsx
+++ next04/lib/posts.tsx
@@ -1,57 +1,59 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { remark } from "remark";
-import html from "remark-html";
+type FileTree = {
+  tree: [
+    {
+      path: string;
+    },
+  ];
+};
 
-const postsDirectory = path.join(process.cwd(), "blogposts");
+export async function getPostByName(
+  fileName: string,
+): Promise<BlogPost | undefined> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/NifemiSoneye/nextBlogPosts/main/${fileName}`,
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    },
+  );
+  if (!res.ok) return undefined;
 
-export function getSortedPostsData() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+  const rawMDX = await res.text();
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-
-    const blogPost: BlogPost = {
-      id,
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-    };
-
-    // Combine the data with the id
-    return blogPost;
-  });
-  // Sort posts by date
-  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+  if (rawMDX === "404: Not Found") return undefined;
 }
 
-export async function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+export async function getPostsMeta(): Promise<Meta[] | undefined> {
+  const res = await fetch(
+    "https://api.github.com/repos/NifemiSoneye/nextBlogPosts/git/trees/main?recursive=1",
+    {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    },
+  );
+  if (!res.ok) return undefined;
 
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
+  const repoFiletree: FileTree = await res.json();
 
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
+  const filesArray = repoFiletree.tree
+    .map((obj) => obj.path)
+    .filter((path) => path.endsWith(".mdx"));
 
-  const contentHtml = processedContent.toString();
+  const posts: Meta[] = [];
 
-  const blogPostWithHTML: BlogPost & { contentHtml: string } = {
-    id,
-    title: matterResult.data.title,
-    date: matterResult.data.date,
-    contentHtml,
-  };
+  for (const file of filesArray) {
+    const post = await getPostByName(file);
+    if (post) {
+      const { meta } = post;
+      posts.push(meta);
+    }
+  }
 
-  // Combine the data with the id
-  return blogPostWithHTML;
+  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
